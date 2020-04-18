481383fa3c64932dd4756d53fc27c95b7f13ba69
Takagi Kensuke <janus.wel.3@gmail.com>
2020-04-19 00:02:31 +0900

    :heavy_plus_sign::wrench: use Prettier

--- lib/frontend-stack.ts
+++ lib/frontend-stack.ts
@@ -1,9 +1,9 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+
+const PROJECT = 'frontend'
 
 export class FrontendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
+    super(scope, id, props)
   }
 }
