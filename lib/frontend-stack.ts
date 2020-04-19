99c2517ea1c25ebcdcce8a9f8d4d1473713893eb
Takagi Kensuke <janus.wel.3@gmail.com>
2020-04-20 00:24:25 +0900

    :arrow_up::heavy_plus_sign: use CloudFront

--- lib/frontend-stack.ts
+++ lib/frontend-stack.ts
@@ -1,15 +1,35 @@
-import * as Cdk from '@aws-cdk/core'
+import * as CDK from '@aws-cdk/core'
 import * as S3 from '@aws-cdk/aws-s3'
+import * as CloudFront from '@aws-cdk/aws-cloudfront'
 
 const PROJECT = 'frontend'
 
-export class FrontendStack extends Cdk.Stack {
-  constructor(scope: Cdk.Construct, id: string, props?: Cdk.StackProps) {
+export class FrontendStack extends CDK.Stack {
+  constructor(scope: CDK.Construct, id: string, props?: CDK.StackProps) {
     super(scope, id, props)
 
-    const bucket = new S3.Bucket(this, PROJECT, {
+    const appBucket = new S3.Bucket(this, `${PROJECT}-bucket`, {
       blockPublicAccess: S3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: Cdk.RemovalPolicy.DESTROY,
+      removalPolicy: CDK.RemovalPolicy.DESTROY,
+    })
+
+    const oai = new CloudFront.OriginAccessIdentity(this, `${PROJECT}-oai`)
+
+    const distribution = new CloudFront.CloudFrontWebDistribution(this, `${PROJECT}-distribution`, {
+      priceClass: CloudFront.PriceClass.PRICE_CLASS_200,
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: appBucket,
+            originAccessIdentity: oai,
+          },
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+            },
+          ],
+        },
+      ],
     })
   }
 }
